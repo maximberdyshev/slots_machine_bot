71e464c443879faad88488ab2d5fe80bb38db8ec
Maxim Berdyshev <36857730+maximberdyshev@users.noreply.github>
2022-11-17 18:17:11 +0500

    добавлен показ статы по очкам и %шанса на триплу

--- src/smb.js
+++ src/smb.js
@@ -10,10 +10,11 @@ let user_stat = []
 smb.on('dice', (ctx) => {
   if (ctx.chat.id != process.env.CHAT_ID) return
 
-  if (ctx.message.forward_date) {
-    ctx.reply('Читы - бан!')
-    return
-  }
+  // античит :D
+//   if (ctx.message.forward_date) {
+//     ctx.reply('Читы - бан!')
+//     return
+//   }
 
   let user = {
     user_id: null,
@@ -82,6 +83,14 @@ smb.command('/my_dices', (ctx) => {
     dice_berries: 0,
     dice_lemons: 0,
     dice_axes: 0,
+    networth: 0,
+  }
+
+  let chance = {
+    alko: 0,
+    berries: 0,
+    lemons: 0,
+    axes: 0,
   }
 
   for (let stat of user_stat) {
@@ -92,16 +101,24 @@ smb.command('/my_dices', (ctx) => {
       response.dice_berries = stat.dice_berries
       response.dice_lemons = stat.dice_lemons
       response.dice_axes = stat.dice_axes
+      
+      response.networth = (stat.dice_alko * 100) + (stat.dice_berries * 100) + (stat.dice_lemons * 100) + (stat.dice_axes * 1000) - (stat.dice_counts * 10)
+
+      chance.alko = ((stat.dice_alko * 100) / stat.dice_counts).toFixed(2)
+      chance.berries = ((stat.dice_berries * 100) / stat.dice_counts).toFixed(2)
+      chance.lemons = ((stat.dice_lemons * 100) / stat.dice_counts).toFixed(2)
+      chance.axes = ((stat.dice_axes * 100) / stat.dice_counts).toFixed(2)
     }
   }
 
   ctx.sendMessage(`${response.user_name}, твоя стата:
-    баланc:  ${response.user_balance}
     бросков:  ${response.dice_counts}
-    бар:  ${response.dice_alko}
-    ягодки:  ${response.dice_berries}
-    лимоны:  ${response.dice_lemons}
-    семерки:  ${response.dice_axes}`)
+    бар:  ${response.dice_alko},  (${chance.alko}%)
+    ягодки:  ${response.dice_berries},  (${chance.berries}%)
+    лимоны:  ${response.dice_lemons},  (${chance.lemons}%)
+    топоры:  ${response.dice_axes},  (${chance.axes}%)
+    баланc:  ${response.user_balance}
+    net worth:  ${response.networth}`)
 })
 
 smb.launch()
