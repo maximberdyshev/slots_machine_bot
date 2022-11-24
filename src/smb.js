73d6f49e50078afe1cc5e63824bc76f670e0ee4a
Maxim Berdyshev <36857730+maximberdyshev@users.noreply.github>
2022-11-24 13:52:14 +0500

    fix

--- src/smb.js
+++ src/smb.js
@@ -60,46 +60,46 @@ smb.on('dice', async (ctx) => {
       chat_id: ctx.message.chat.id,
     }
 
-    let sendID
+    // let sendID
 
-    // реплики бота на триплы
-    // TODO: повесить рандомайзер
-    if (ctx.message.dice.value == 64) {
-      sendID = await ctx.sendSticker(
-        'CAACAgIAAxkBAAIChWN7Xu8rD0Hjd5C7xCFajMPiCs-cAAJYFAACMET4SPA1u80JntoQKwQ',
-        ctx.message.from.id
-      )
-      oneSlot.message_id = sendID.message_id
-      oneSlot.date = new Date().getTime()
-      slotLimit.push(oneSlot)
-    }
-    if (ctx.message.dice.value == 1) {
-      sendID = await ctx.sendSticker(
-        'CAACAgIAAxkBAAICgGN7XTAotgHhdvlyT4pjM5ZeavokAALXGgACAldIScihT69U4hKHKwQ',
-        ctx.message.from.id
-      )
-      oneSlot.message_id = sendID.message_id
-      oneSlot.date = new Date().getTime()
-      slotLimit.push(oneSlot)
-    }
-    if (ctx.message.dice.value == 22) {
-      sendID = await ctx.sendSticker(
-        'CAACAgIAAxkBAAICg2N7XrjeZ4rr5HCgahipIY-_ecYCAAJ8EQACTbD4SGTMci63kLrWKwQ',
-        ctx.message.from.id
-      )
-      oneSlot.message_id = sendID.message_id
-      oneSlot.date = new Date().getTime()
-      slotLimit.push(oneSlot)
-    }
-    if (ctx.message.dice.value == 43) {
-      sendID = await ctx.sendSticker(
-        'CAACAgIAAxkBAAIChGN7Xt_GWDMsDdYhC8I0i_qdSREJAAKiDwACIVD4SF2L5ep3b5-EKwQ',
-        ctx.message.from.id
-      )
-      oneSlot.message_id = sendID.message_id
-      oneSlot.date = new Date().getTime()
-      slotLimit.push(oneSlot)
-    }
+    // // реплики бота на триплы
+    // // TODO: повесить рандомайзер
+    // if (ctx.message.dice.value == 64) {
+    //   sendID = await ctx.sendSticker(
+    //     'CAACAgIAAxkBAAIChWN7Xu8rD0Hjd5C7xCFajMPiCs-cAAJYFAACMET4SPA1u80JntoQKwQ',
+    //     ctx.message.from.id
+    //   )
+    //   oneSlot.message_id = sendID.message_id
+    //   oneSlot.date = new Date().getTime()
+    //   slotLimit.push(oneSlot)
+    // }
+    // if (ctx.message.dice.value == 1) {
+    //   sendID = await ctx.sendSticker(
+    //     'CAACAgIAAxkBAAICgGN7XTAotgHhdvlyT4pjM5ZeavokAALXGgACAldIScihT69U4hKHKwQ',
+    //     ctx.message.from.id
+    //   )
+    //   oneSlot.message_id = sendID.message_id
+    //   oneSlot.date = new Date().getTime()
+    //   slotLimit.push(oneSlot)
+    // }
+    // if (ctx.message.dice.value == 22) {
+    //   sendID = await ctx.sendSticker(
+    //     'CAACAgIAAxkBAAICg2N7XrjeZ4rr5HCgahipIY-_ecYCAAJ8EQACTbD4SGTMci63kLrWKwQ',
+    //     ctx.message.from.id
+    //   )
+    //   oneSlot.message_id = sendID.message_id
+    //   oneSlot.date = new Date().getTime()
+    //   slotLimit.push(oneSlot)
+    // }
+    // if (ctx.message.dice.value == 43) {
+    //   sendID = await ctx.sendSticker(
+    //     'CAACAgIAAxkBAAIChGN7Xt_GWDMsDdYhC8I0i_qdSREJAAKiDwACIVD4SF2L5ep3b5-EKwQ',
+    //     ctx.message.from.id
+    //   )
+    //   oneSlot.message_id = sendID.message_id
+    //   oneSlot.date = new Date().getTime()
+    //   slotLimit.push(oneSlot)
+    // }
 
     // ctx.sendSticker('CAACAgIAAxkBAAICgGN7XTAotgHhdvlyT4pjM5ZeavokAALXGgACAldIScihT69U4hKHKwQ','306979269') база
     // CAACAgIAAxkBAAICg2N7XrjeZ4rr5HCgahipIY-_ecYCAAJ8EQACTbD4SGTMci63kLrWKwQ ахуителен
